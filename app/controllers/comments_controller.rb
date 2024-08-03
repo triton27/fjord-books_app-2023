1e5fae0fc345c23c750dcf00fb9444afb08655f5
Junichi Ito <jit@sonicgarden.jp>
2024-08-03 19:40:49 +0900

    turbo streamでコメントを削除

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,9 +2,8 @@
 
 class CommentsController < ApplicationController
   def destroy
-    comment = current_user.comments.find(params[:id])
-    commentable = comment.commentable
-    comment.destroy
-    redirect_to commentable, notice: t('controllers.common.notice_destroy', name: Comment.model_name.human)
+    @comment = current_user.comments.find(params[:id])
+    @commentable = @comment.commentable
+    @comment.destroy
   end
 end
