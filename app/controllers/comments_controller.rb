84f2821c152b1c1f19388d531a3542cb5dc8be97
Junichi Ito <jit@sonicgarden.jp>
2024-08-03 20:24:46 +0900

    turbo streamでコメントを更新

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,28 @@
 # frozen_string_literal: true
 
 class CommentsController < ApplicationController
+  before_action :set_comment
+
+  def edit
+  end
+
+  def update
+    # NOTE: 実装を複雑にしないよう、あえて保存失敗時の処理を考慮しないことにした（失敗時はシステムエラーとする）
+    @comment.update!(comment_params)
+  end
+
   def destroy
-    @comment = current_user.comments.find(params[:id])
     @commentable = @comment.commentable
     @comment.destroy
+  end
+
+  private
+
+  def set_comment
+    @comment = current_user.comments.find(params[:id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content)
   end
 end
